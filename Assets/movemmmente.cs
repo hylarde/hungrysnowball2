540377d8143686e4655b64ccd19b8fba6062a03d
VIC0243347 <VIC0243347@SEN622596>
2026-07-28 16:48:36 -0300

    configuração do personagem

--- Assets/movemmmente.cs
+++ Assets/movemmmente.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class movemmmente : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int speed;
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Input.GetAxis("Horizontal");
         
     }
 }
